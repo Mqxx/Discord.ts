3c87565ac8dd47dcbd11d438f03ab19de1224845
Mqx <62719703+Mqxx@users.noreply.github.com>
2022-09-27 20:27:27 +0200

    Delete index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-const token = Deno.readTextFileSync('./discord.token').trim()
-
-import GatewayManager from './src/gateway/GatewayManager.ts'
-
-const gwm = new GatewayManager()
-
-gwm.connect(token, 0b1111111111111111111111)
